a015167aea640581eb40814fc6642815bb5acfd7
ajay patil <ajay patil>
2017-05-21 11:31:41 -0400

    home page updated

--- Movie-Star-App/src/app/app.module.ts
+++ Movie-Star-App/src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { MovieListComponent } from './movies/movie-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MovieListComponent
   ],
   imports: [
     BrowserModule,
---
+++ Movie-Star-App/src/app/movies/movie-list.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  moduleId: module.id,
+  selector: 'pm-movies',
+  templateUrl: './movie-list.component.html',
+  styleUrls: ['./movie-list.component.css']
+})
+export class MovieListComponent {
+    
+}
